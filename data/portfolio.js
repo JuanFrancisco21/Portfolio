3d465186899a55a668f044498e1be44b1b085dae
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2022-12-25 21:50:09 +0100

    Datos del portfolio y blog con fotos

--- data/portfolio.js
+++ data/portfolio.js
@@ -1,115 +1,125 @@
 module.exports = [
   {
     id: 1,
-    tooltipId: "WEB KAZE",
-    portfolioImage: "img/portfolio/1.jpg",
-    portfolioLink: "https://juanfrancisco21.github.io/K-A-Z-E/",
-    title: "WEB KAZE",
-    meta: "HTML",
-    clientName: "Miguel Carmona",
-    category: "Desarrollo Interfaces",
-    date: "Noviembre 22, 2022",
+    tooltipId: 'WEB KAZE',
+    portfolioImage: 'img/portfolio/1.jpg',
+    portfolioLink: 'https://juanfrancisco21.github.io/K-A-Z-E/',
+    title: 'WEB KAZE',
+    meta: 'HTML',
+    clientName: 'Miguel Carmona',
+    category: 'Desarrollo Interfaces',
+    date: 'Noviembre 22, 2022',
     projectDescriptions: (
       <>
         <p>
-          Este es un proyecto, que surje de una actividad de Diseño de Interfaces en la cual se nos 
-          requiere de realizar una página web de un cantante que nosotros elijamos. Todo ello lo 
-          desarrollariamos con tecnologias tan simples y tan usadas como son HTML, CSS, JS. 
+          Este es un proyecto, que surje de una actividad de Diseño de
+          Interfaces en la cual se nos requiere de realizar una página web de un
+          cantante que nosotros elijamos. Todo ello lo desarrollariamos con
+          tecnologias tan simples y tan usadas como son HTML, CSS, JS.
         </p>
 
         <p>
-          Mi Proyecto se basó en el cantante KAZE, rapero español - para ello se diseñan y 
-          crean 3 vistas. La página de Inicio, Blog y Galeria. Una caracteristica que 
-          era importante para este proyecto era la posibilidad de reproducir las caciones 
-          de cierto album nuevo, todo ello sin salir de la misma Web, al cual le añadí 
-          la posibilidad de leer la letra de dichas canciones.
+          Mi Proyecto se basó en el cantante KAZE, rapero español - para ello se
+          diseñan y crean 3 vistas. La página de Inicio, Blog y Galeria. Una
+          caracteristica que era importante para este proyecto era la
+          posibilidad de reproducir las caciones de cierto album nuevo, todo
+          ello sin salir de la misma Web, al cual le añadí la posibilidad de
+          leer la letra de dichas canciones.
         </p>
       </>
     ),
   },
   {
     id: 2,
-    tooltipId: "Cofradia  Nazarenos",
-    portfolioImage: "img/portfolio/2.jpg",
-    portfolioLink: "https://juanfrancisco21.github.io/Cofradia/",
-    title: "COFRADIA NAZARENOS",
-    meta: "BOOTSTRAP",
-    clientName: "Miguel Carmona",
-    category: "Desarrollo Interfaces",
-    date: "Diciembre 16, 2022",
+    tooltipId: 'Cofradia  Nazarenos',
+    portfolioImage: 'img/portfolio/2.jpg',
+    portfolioLink: 'https://juanfrancisco21.github.io/Cofradia/',
+    title: 'COFRADIA NAZARENOS',
+    meta: 'BOOTSTRAP',
+    clientName: 'Miguel Carmona',
+    category: 'Desarrollo Interfaces',
+    date: 'Diciembre 16, 2022',
     projectDescriptions: (
       <>
         <p>
-          We live in a world where we need to move quickly and iterate on our
-          ideas as flexibly as possible. Building mockups strikes the ideal
-          balance between true-life representation of the end product and ease
-          of modification.
+          Todo este proyecto comienza como una actividad para el grado superior
+          DAW. Donde partimos del problema proporcionado por el profesor de
+          crear una Página web basada en el color morado. Con la cual el mismo
+          hizo una sugerencia de hacerla basada en una cofradia. Una vez que
+          teníamos el problema planteado, comenzamos a trabajar en la solución.
+          Primero, se hizo una investigación sobre el color morado y su
+          simbolismo en la cultura y la historia descubriendo que el color
+          morado ha sido asociado con la realeza, la sabiduría y la
+          espiritualidad en muchas culturas a lo largo de la historia. También
+          encontramos que en muchas cofradías, el color morado se utiliza como
+          símbolo de penitencia y humildad.
         </p>
-
         <p>
-          Mockups are useful both for the creative phase of the project - for
-          instance when you&lsquo;re trying to figure out your user flows or the
-          proper visual hierarchy - and the production phase when they will
-          represent the target product. Making mockups a part of your creative
-          and development process allows you to quickly and easily ideate.
+          Con esta información en mente, decidimos enfocar nuestra página web en
+          el simbolismo del color morado en la historia y en la cultura de las
+          cofradías. Creamos un diseño atractivo y fácil de usar que destacaba
+          el color morado y utilizaba imágenes y gráficos para ilustrar el
+          simbolismo del color en diferentes contextos históricos y culturales.
+        </p>
+        <p>
+          Finalizando la web con tecnologias como principales tecnologias HTML5,
+          SCSS Y BOOTSTRAP. Todo ello cumpliendo los estandares, tanto de
+          accesibilidad como de usabilidad definidos por WCAG, realizando
+          pruebas con la herramienta WAVE, a la par que comprobaciones con
+          lectura de pantalla para personas con capacidad reducida, o el uso de
+          heatmaps para comprobrar la usabilidad.
         </p>
       </>
     ),
   },
   {
     id: 3,
-    tooltipId: "Banco",
-    portfolioImage: "img/portfolio/3.jpg",
-    portfolioLink: "https://www.facebook.com/",
-    title: "SCAMMER BANK",
-    meta: "CLI - SERV",
-    clientName: "Juan Francisco",
-    category: "Conexiónes TCP",
-    date: "Febrero 02, 2021",
+    tooltipId: 'Banco',
+    portfolioImage: 'img/portfolio/3.jpg',
+    portfolioLink: 'https://github.com/JuanFrancisco21/Ud3-Sockets',
+    title: 'SCAMMER BANK',
+    meta: 'CLI - SERV',
+    clientName: 'Juan Francisco',
+    category: 'Conexiónes TCP',
+    date: 'Febrero 02, 2021',
     projectDescriptions: (
       <>
         <p>
-          We live in a world where we need to move quickly and iterate on our
-          ideas as flexibly as possible. Building mockups strikes the ideal
-          balance between true-life representation of the end product and ease
-          of modification.
+        Lo que se pretende con este proyecto es desarrollar una aplicación cliente-servidor utilizando Java
+         y conexión TCP, con el objetivo de cumplir con los requisitos de una asignatura y demostrar la capacidad de crear software funcional.
         </p>
 
         <p>
-          Mockups are useful both for the creative phase of the project - for
-          instance when you&lsquo;re trying to figure out your user flows or the
-          proper visual hierarchy - and the production phase when they will
-          represent the target product. Making mockups a part of your creative
-          and development process allows you to quickly and easily ideate.
+        La aplicación debe permitir que el cliente envíe y reciba información del servidor, utilizando la conexión TCP
+         para establecer la comunicación entre ambos. Es importante  asegurarse de que la aplicación funcione correctamente y sea fácil de usar para el cliente. 
+        Además, es necesario tener en cuenta aspectos como la seguridad y la escalabilidad de la aplicación para garantizar su sostenibilidad a largo plazo.
         </p>
       </>
     ),
   },
   {
     id: 4,
-    tooltipId: "Wishlist",
-    portfolioImage: "img/portfolio/4.jpg",
-    portfolioLink: "https://github.com/JuanFrancisco21/React_WishList",
-    title: "WISHLIST",
-    meta: "REACT",
-    clientName: "Juan Francisco",
-    category: "Entorno Cliente",
-    date: "Octubre 22, 2022",
+    tooltipId: 'Wishlist',
+    portfolioImage: 'img/portfolio/4.jpg',
+    portfolioLink: 'https://github.com/JuanFrancisco21/React_WishList',
+    title: 'WISHLIST',
+    meta: 'REACT',
+    clientName: 'Juan Francisco',
+    category: 'Entorno Cliente',
+    date: 'Octubre 22, 2022',
     projectDescriptions: (
       <>
         <p>
-          We live in a world where we need to move quickly and iterate on our
-          ideas as flexibly as possible. Building mockups strikes the ideal
-          balance between true-life representation of the end product and ease
-          of modification.
+        Este es el primer proyecto desarrollado con el framework react "La lista de deseos es una aplicación sencilla 
+        pero muy útil para llevar un registro de las cosas que queremos adquirir o hacer en el futuro. Con React, hemos podido 
+        crear una interfaz intuitiva y fácil de usar, lo que hace que sea muy agradable de utilizar. 
         </p>
 
         <p>
-          Mockups are useful both for the creative phase of the project - for
-          instance when you&lsquo;re trying to figure out your user flows or the
-          proper visual hierarchy - and the production phase when they will
-          represent the target product. Making mockups a part of your creative
-          and development process allows you to quickly and easily ideate.
+        Además, hemos implementado algunas características avanzadas, como la posibilidad de marcar los elementos de la lista como
+         completados y de eliminarlos de manera sencilla con solo un clic. También hemos añadido la opción de filtrar la lista por 
+         diferentes categorías para poder encontrar  más fácilmente lo que buscamos. En resumen, este proyecto con React ha sido 
+         una gran oportunidad para aprender y poner en práctica nuestros conocimientos en el desarrollo de aplicaciones web modernas.
         </p>
       </>
     ),

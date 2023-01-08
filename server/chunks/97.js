22aae30a8fd3c7d140308f47c8306142f4308d18
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2023-01-09 00:14:20 +0100

    Updates

---
+++ server/chunks/97.js
@@ -0,0 +1,57 @@
+"use strict";
+exports.id = 97;
+exports.ids = [97];
+exports.modules = {
+
+/***/ 97:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(968);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_1__);
+
+
+const Seo = ({ pageTitle , font  })=>/*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)((next_head__WEBPACK_IMPORTED_MODULE_1___default()), {
+            children: [
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("title", {
+                    children: "Juan Francisco Aguilar S\xe1nchez - Desarrollador Software"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+                    httpEquiv: "x-ua-compatible",
+                    content: "ie=edge"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+                    name: "keywords",
+                    content: " cv, portfolio, react nextjs portfolio, resumen"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+                    name: "description",
+                    content: "Juan Francisco Aguilar S\xe1nchez - Personal Portfolio"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+                    name: "viewport",
+                    content: "width=device-width, initial-scale=1, shrink-to-fit=no"
+                }),
+                font && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
+                    href: font,
+                    rel: "stylesheet"
+                }),
+                /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
+                    rel: "icon",
+                    href: "Logo.ico"
+                })
+            ]
+        })
+    });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Seo);
+
+
+/***/ })
+
+};
+;

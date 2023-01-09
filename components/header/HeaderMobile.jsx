882a50d9b040b4bb8b467e1e7cdca53eefe00a91
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2023-01-09 19:47:30 +0100

    Mobile photo fix

--- components/header/HeaderMobile.jsx
+++ components/header/HeaderMobile.jsx
@@ -6,8 +6,8 @@ const HeaderMobile = () => {
     <>
       <div className="logo">
         <Link href="/">
-          <Image className="logo_light" src='/img/logo/dark.png' alt="brand" width="92" height="37"/>
-          <Image className="logo_dark" src='/img/logo/dark.png' alt="brand" width="92" height="37"/>
+          <img className="logo_light" src={'img/logo/dark.png'} alt="brand" width="92" height="37"/>
+          <img className="logo_dark" src={'img/logo/dark.png'} alt="brand" width="92" height="37"/>
         </Link>
       </div>
       {/* End .logo */}
